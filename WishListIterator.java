fb2b957bc61c4434e96237248dcdae67a638ac37
ddkeen <d3keen@gmail.com>
2021-02-09 10:55:21 -0500

    manually fix indentation

--- WishListIterator.java
+++ WishListIterator.java
@@ -7,33 +7,33 @@ import java.util.Iterator;
 public class WishListIterator implements Iterator {
     
     private Item[] items;
-	private int position = 0;
+    private int position = 0;
     
     /**
      * WishListIterator constructor to set the items array
      * @param items an array of Item objects
      */
-	public WishListIterator(Item[] items) {
-		this.items = items;
-	}
+    public WishListIterator(Item[] items) {
+	    this.items = items;
+    }
     
     /**
      * Iteratros to the next item in the array if there is one and returns it, returns null otherwise
      * @return item, the next item. returns null if there is no next item
      */
-	public Item next() {
+    public Item next() {
         Item item = items[position];
         if(item == null) {
             return null;
         }
-		position++;
-		return item;
-	}
-	/**
+	    position++;
+	    return item;
+    }
+    /**
      * Returns whether there are more items to iterate over in the array
      * @return boolean, true if there are more items, false if there are no more items
      */
-	public boolean hasNext() {
-		return !(position >= items.length || items[position] == null);
-	}
+    public boolean hasNext() {
+	    return !(position >= items.length || items[position] == null);
+    }
 }
